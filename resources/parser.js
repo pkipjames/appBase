d7d0ed4c79505597ee1e61fb957b7ddaeb4aaf78
James M <45425618+pkipjames@users.noreply.github.com>
2019-10-17 12:47:41 -0400

    Update parser.js

--- resources/parser.js
+++ resources/parser.js
@@ -1,163 +1,159 @@
-function parseExpression(program) {
+function runApp(url,div){
+  function parseExpression(program) {
   program = skipSpace(program);
-  let match, expr;
-  if (match = /^"([^"]*)"/.exec(program)) {
+  var match, expr;
+  if (match = /^"([^"]*)"/.exec(program))
     expr = {type: "value", value: match[1]};
-  } else if (match = /^\d+\b/.exec(program)) {
+  else if (match = /^\d+\b/.exec(program))
     expr = {type: "value", value: Number(match[0])};
-  } else if (match = /^[^\s(),#"]+/.exec(program)) {
+  else if (match = /^[^\s(),"]+/.exec(program))
     expr = {type: "word", name: match[0]};
-  } else {
+  else
     throw new SyntaxError("Unexpected syntax: " + program);
-  }
 
   return parseApply(expr, program.slice(match[0].length));
 }
 
 function skipSpace(string) {
-  let first = string.search(/\S/);
+  var first = string.search(/\S/);
   if (first == -1) return "";
   return string.slice(first);
 }
 
 function parseApply(expr, program) {
   program = skipSpace(program);
-  if (program[0] != "(") {
+  if (program[0] != "(")
     return {expr: expr, rest: program};
-  }
 
   program = skipSpace(program.slice(1));
   expr = {type: "apply", operator: expr, args: []};
   while (program[0] != ")") {
-    let arg = parseExpression(program);
+    var arg = parseExpression(program);
     expr.args.push(arg.expr);
     program = skipSpace(arg.rest);
-    if (program[0] == ",") {
+    if (program[0] == ",")
       program = skipSpace(program.slice(1));
-    } else if (program[0] != ")") {
+    else if (program[0] != ")")
       throw new SyntaxError("Expected ',' or ')'");
-    }
   }
   return parseApply(expr, program.slice(1));
 }
 
 function parse(program) {
-  let {expr, rest} = parseExpression(program);
-  if (skipSpace(rest).length > 0) {
+  var result = parseExpression(program);
+  if (skipSpace(result.rest).length > 0)
     throw new SyntaxError("Unexpected text after program");
-  }
-  return expr;
+  return result.expr;
 }
 //    operator: {type: "word", name: "+"},
 //    args: [{type: "word", name: "a"},
 //           {type: "value", value: 10}]}
 
-var specialForms = Object.create(null);
+function evaluate(expr, env) {
+  switch(expr.type) {
+    case "value":
+      return expr.value;
 
-function evaluate(expr, scope) {
-  if (expr.type == "value") {
-    return expr.value;
-  } else if (expr.type == "word") {
-    if (expr.name in scope) {
-      return scope[expr.name];
-    } else {
-      throw new ReferenceError(
-        `Undefined binding: ${expr.name}`);
-    }
-  } else if (expr.type == "apply") {
-    let {operator, args} = expr;
-    if (operator.type == "word" &&
-        operator.name in specialForms) {
-      return specialForms[operator.name](expr.args, scope);
-    } else {
-      let op = evaluate(operator, scope);
-      if (typeof op == "function") {
-        return op(...args.map(arg => evaluate(arg, scope)));
-      } else {
+    case "word":
+      if (expr.name in env)
+        return env[expr.name];
+      else
+        throw new ReferenceError("Undefined variable: " +
+                                 expr.name);
+    case "apply":
+      if (expr.operator.type == "word" &&
+          expr.operator.name in specialForms)
+        return specialForms[expr.operator.name](expr.args,
+                                                env);
+      var op = evaluate(expr.operator, env);
+      if (typeof op != "function")
         throw new TypeError("Applying a non-function.");
-      }
-    }
+      return op.apply(null, expr.args.map(function(arg) {
+        return evaluate(arg, env);
+      }));
   }
 }
 
-specialForms.if = (args, scope) => {
-  if (args.length != 3) {
-    throw new SyntaxError("Wrong number of args to if");
-  } else if (evaluate(args[0], scope) !== false) {
-    return evaluate(args[1], scope);
-  } else {
-    return evaluate(args[2], scope);
-  }
+var specialForms = Object.create(null);
+
+specialForms["if"] = function(args, env) {
+  if (args.length != 3)
+    throw new SyntaxError("Bad number of args to if");
+
+  if (evaluate(args[0], env) !== false)
+    return evaluate(args[1], env);
+  else
+    return evaluate(args[2], env);
 };
 
-specialForms.while = (args, scope) => {
-  if (args.length != 2) {
-    throw new SyntaxError("Wrong number of args to while");
-  }
-  while (evaluate(args[0], scope) !== false) {
-    evaluate(args[1], scope);
-  }
+specialForms["while"] = function(args, env) {
+  if (args.length != 2)
+    throw new SyntaxError("Bad number of args to while");
+
+  while (evaluate(args[0], env) !== false)
+    evaluate(args[1], env);
 
   // Since undefined does not exist in Egg, we return false,
   // for lack of a meaningful result.
   return false;
 };
 
-specialForms.do = (args, scope) => {
-  let value = false;
-  for (let arg of args) {
-    value = evaluate(arg, scope);
-  }
+specialForms["do"] = function(args, env) {
+  var value = false;
+  args.forEach(function(arg) {
+    value = evaluate(arg, env);
+  });
   return value;
 };
 
-specialForms.define = (args, scope) => {
-  if (args.length != 2 || args[0].type != "word") {
-    throw new SyntaxError("Incorrect use of define");
-  }
-  let value = evaluate(args[1], scope);
-  scope[args[0].name] = value;
+specialForms["define"] = function(args, env) {
+  if (args.length != 2 || args[0].type != "word")
+    throw new SyntaxError("Bad use of define");
+  var value = evaluate(args[1], env);
+  env[args[0].name] = value;
   return value;
 };
 
-var topScope = Object.create(null);
+var topEnv = Object.create(null);
 
-topScope.true = true;
-topScope.false = false;
+topEnv["true"] = true;
+topEnv["false"] = false;
 
-for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
-  topScope[op] = Function("a, b", `return a ${op} b;`);
-}
+["+", "-", "*", "/", "==", "<", ">"].forEach(function(op) {
+  topEnv[op] = new Function("a, b", "return a " + op + " b;");
+});
 
-topScope.print = value => {
+topEnv["print"] = function(value) {
   console.log(value);
   return value;
 };
 
-function run(program) {
-  return evaluate(parse(program), Object.create(topScope));
+function run() {
+  var env = Object.create(topEnv);
+  var program = Array.prototype.slice
+    .call(arguments, 0).join("\n");
+  return evaluate(parse(program), env);
 }
 
-specialForms.fun = (args, scope) => {
-  if (!args.length) {
+specialForms["fun"] = function(args, env) {
+  if (!args.length)
     throw new SyntaxError("Functions need a body");
-  }
-  let body = args[args.length - 1];
-  let params = args.slice(0, args.length - 1).map(expr => {
-    if (expr.type != "word") {
-      throw new SyntaxError("Parameter names must be words");
-    }
+  function name(expr) {
+    if (expr.type != "word")
+      throw new SyntaxError("Arg names must be words");
     return expr.name;
-  });
+  }
+  var argNames = args.slice(0, args.length - 1).map(name);
+  var body = args[args.length - 1];
 
   return function() {
-    if (arguments.length != params.length) {
+    if (arguments.length != argNames.length)
       throw new TypeError("Wrong number of arguments");
-    }
-    let localScope = Object.create(scope);
-    for (let i = 0; i < arguments.length; i++) {
-      localScope[params[i]] = arguments[i];
-    }
-    return evaluate(body, localScope);
+    var localEnv = Object.create(env);
+    for (var i = 0; i < arguments.length; i++)
+      localEnv[argNames[i]] = arguments[i];
+    return evaluate(body, localEnv);
   };
 };
+  return run(url);
+}
